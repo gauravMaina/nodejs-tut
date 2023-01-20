e5f48dd633b12364a5a6d900d8390733e876b117
Gaurav Jadhav <gauravj@octallocal.com>
2023-01-20 14:30:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    x:5,
+    y:8,
+    z:function(){
+        return x +y
+    }
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+
+const {x,y,z} = require('./app')
+// console.log("y",y)
+const array = [ 2,5,9,0,3]
+
+ const result = array.filter((item)=>item>3)
+ console.log("result",result)
