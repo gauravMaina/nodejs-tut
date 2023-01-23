572ad7bdf56a1c41d30a2bf7ed70375f0e6476d6
Gaurav Jadhav <you@example.com>
2023-01-23 16:39:55 +0530

    crud operation for fs module

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const fs = require('fs')
 const { join } = require('path')
 
-const dirPath = join(__dirname, 'files')
-
-for (let i = 1; i <= 5; i++) {
-  fs.writeFileSync(
-    `${dirPath}/hello${i}.txt`,
-    `hello this is file name is hello${i}.txt`
-  )
-}
-
-fs.readdir(dirPath, (err, files) => {
-  if (err) {
-    console.log('error is', err)
-    return
-  }
-  files.forEach(item => {
-    console.log(item)
-  })
+const dirPath = join(__dirname, 'crud')
+const filePath = `${dirPath}/apple.txt`
+// fs.writeFileSync(`${dirPath}/apple.txt`, 'this is file name is apple.txt')
+// fs.appendFile(filePath, ' new data updated', err => {
+//   if (!err) console.log(' data updated succefully')
+// })
+// fs.rename(filePath, `${dirPath}/fruit.txt`, err => {
+//   if (!err) {
+//     console.log('filname is Changed')
+//   }
+// })
+fs.unlink(`${dirPath}/fruit.txt`, err => {
+  if (err) throw err
+  console.log('fruit.txt was deleted')
 })
