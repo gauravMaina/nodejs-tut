6e7f49d5f41e87231fa51edcee0795c7f5fa450f
Gaurav Jadhav <you@example.com>
2023-01-23 17:57:27 +0530

    practice 2

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-// console.log('first console.log')
-// setTimeout(() => {
-//   console.log('Second console.log')
-// }, 2000)
-// console.log('third console.log')
 const a = 10
 let b = 0
- setTimeout(() => {
-  b =10
-}, 2000);
 
-console.log(a+b);
+const waitData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(20)
+  }, 2000)
+});
+(async () => {
+  b = await waitData
+  console.log(a + b)
+})()
+// waitData.then(data => {
+//   console.log(a + data)
+// })
