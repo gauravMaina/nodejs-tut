2b3fe04ed75ac2c5ddf3030192ec10f317529093
Gaurav Jadhav <you@example.com>
2023-01-24 10:51:45 +0530

    handling  req and res

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-  res.send('<h1>hello, this about page</h1>')
+  res.send(`<h1> Welcome ,${req.query.name} ${req.query.lastname}</h1>`)
 })
 app.get('/help', (req, res) => {
-  res.send('<h1>hello, this help page</h1>')
+  res.send({
+    name: 'Gaurav',
+    age: 23,
+    gender: 'Male'
+  })
 })
 app.get('/about', (req, res) => {
   res.send('<h1>hello, this about page</h1>')
