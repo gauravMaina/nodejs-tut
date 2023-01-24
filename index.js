7b0b2f6fd7806000e650d9dd91928b4d62aae1d2
Gaurav Jadhav <you@example.com>
2023-01-24 11:57:56 +0530

    redering html pages and remover extension

--- index.js
+++ index.js
@@ -2,5 +2,18 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const publicPath = path.join(__dirname, 'public')
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('', (req, res) => {
+  res.sendFile(`${publicPath}/index.html`)
+})
+app.get('/help', (req, res) => {
+  res.sendFile(`${publicPath}/help.html`)
+})
+app.get('/about', (req, res) => {
+  res.sendFile(`${publicPath}/about.html`)
+})
+app.get('*', (req, res) => {
+  res.sendFile(`${publicPath}/pageNotFound.html`)
+})
 app.listen(9000)
